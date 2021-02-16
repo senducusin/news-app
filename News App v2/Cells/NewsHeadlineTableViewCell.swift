018db4f03de027d6bf766e278c991c6b86e08632
Jansen Ducusin <senducusin@gmail.com>
2021-02-16 18:28:07 +0800

    Created a custom cell, NewsHeadlineTableViewCell

---
+++ News App v2/Cells/NewsHeadlineTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NewsHeadlineTableViewCell.swift
+//  News App v2
+//
+//  Created by Jansen Ducusin on 2/16/21.
+//
+
+import Foundation
+import UIKit
+
+class NewsHeadlineTableViewCell:    UITableViewCell{
+    
+    @IBOutlet weak var titleLabel:  UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var headlineImageView:   UIImageView!
+    
+}
