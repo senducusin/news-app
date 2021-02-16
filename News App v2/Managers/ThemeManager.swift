34f7a8a1d2a4e5a7c2d070d6596f90263512bec6
Jansen Ducusin <senducusin@gmail.com>
2021-02-16 18:14:18 +0800

    Updated ThemeManager and fix theme colors

--- News App v2/Managers/ThemeManager.swift
+++ News App v2/Managers/ThemeManager.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ThemeManager{
     static func setup(){
-        UINavigationBar.appearance().barTintColor = .darkGray
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = .darkGray
+        
+        navigationBarAppearance.titleTextAttributes = [.foregroundColor:UIColor.white]
+        navigationBarAppearance.largeTitleTextAttributes = [.foregroundColor:UIColor.white]
+        
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+        UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
     }
 }
