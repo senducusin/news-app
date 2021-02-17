227fd4413e8162047329976d026b2a428bf4b522
Jansen Ducusin <senducusin@gmail.com>
2021-02-17 15:50:04 +0800

    Code cleanup

--- News App v2/Extensions/UIImage+Extensions.swift
+++ News App v2/Extensions/UIImage+Extensions.swift
@@ -14,26 +14,4 @@ extension UIImage {
     static func imageForPlaceholder() -> UIImage {
         return UIImage(systemName: "photo.fill")!
     }
-    
-//    static func imageForHeadline(url: String, completion:@escaping(UIImage) -> ()){
-//        guard let imageURL = URL(string: url) else {
-//            DispatchQueue.main.async {
-//                completion(UIImage.imageForPlaceholder())
-//            }
-//            return
-//        }
-//
-//        DispatchQueue.main.async {
-//            let imageView:UIImageView = UIImageView.kf.setImage(with: imageURL)
-//            completion(imageView.image)
-//        }
-        
-//        if let data = try? Data(contentsOf: imageURL){
-//            if let downloadImage = UIImage(data: data) {
-//                DispatchQueue.main.async {
-//                    completion(downloadImage)
-//                }
-//            }
-//        }
-//    }
 }
