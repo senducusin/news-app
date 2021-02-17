c3d7094ab8a9c9e36d7f607a5a7252431cb3cc85
Jansen Ducusin <senducusin@gmail.com>
2021-02-17 11:46:06 +0800

    Removed all dummy data

--- News App v2/Controllers/NewsHeadlineTableViewController.swift
+++ News App v2/Controllers/NewsHeadlineTableViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 
 class NewsHeadlineTableViewController: UITableViewController{
     
-    let news = [
-        ["title":"Dummy Title",
-         "description":"This is a sample description"],
-        ["title":"Dummy Title Dummy Title Dummy Title Dummy Title",
-         "description":"This is a sample description This is a sample description This is a sample description This is a sample description"]
-    ]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,22 +22,5 @@ class NewsHeadlineTableViewController: UITableViewController{
 }
 
 extension NewsHeadlineTableViewController{
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return news.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsHeadlineTableViewCell", for: indexPath) as? NewsHeadlineTableViewCell else {
-            fatalError("NewsHeadlineTableViewCell not found! ")
-        }
-        
-        cell.titleLabel.text = self.news[indexPath.row]["title"]!
-        cell.descriptionLabel.text = self.news[indexPath.row]["description"]!
-        
-        return cell
-    }
 }
