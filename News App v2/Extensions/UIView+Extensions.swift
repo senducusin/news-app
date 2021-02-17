b81f0ed7857709c2cbf5cc51e0717f588ee8aabd
Jansen Ducusin <senducusin@gmail.com>
2021-02-17 16:40:07 +0800

    UI retouch

--- News App v2/Extensions/UIView+Extensions.swift
+++ News App v2/Extensions/UIView+Extensions.swift
@@ -12,12 +12,13 @@ extension UIView {
     static func viewForTableViewHeader(title: String) -> UIView {
         let screenRect = UIScreen.main.bounds
         let substitleView = UIView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: screenRect.size.width, height: 44)))
+        substitleView.backgroundColor = UIColor(red: 0.96, green: 0.96, blue: 0.98, alpha: 1.00)
         
         let subtitleLabel = UILabel(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: screenRect.width, height: 44)))
         
         subtitleLabel.text = title
         subtitleLabel.textAlignment = .center
-        subtitleLabel.textColor = .gray
+        subtitleLabel.textColor = .lightGray
         
         substitleView.addSubview(subtitleLabel)
         return substitleView
@@ -31,7 +32,7 @@ extension UIView {
         
         let sectionHeaderLabel = UILabel(frame: headerView.frame)
         sectionHeaderLabel.text = title.uppercased()
-        sectionHeaderLabel.textColor = .black
+        sectionHeaderLabel.textColor = UIColor(red: 0.00, green: 0.66, blue: 1.00, alpha: 1.00)
         sectionHeaderLabel.font = UIFont.fontForSectionHeaderTitle()
         
         headerView.addSubview(sectionHeaderLabel)
--- News App v2/Managers/ThemeManager.swift
+++ News App v2/Managers/ThemeManager.swift
@@ -11,12 +11,12 @@ import UIKit
 class ThemeManager{
     static func setup(){
         let navigationBarAppearance = UINavigationBarAppearance()
-        navigationBarAppearance.backgroundColor = .darkGray
+        navigationBarAppearance.backgroundColor = UIColor(red: 0.21, green: 0.23, blue: 0.28, alpha: 1.00)
         
-        navigationBarAppearance.titleTextAttributes = [.foregroundColor:UIColor.white]
-        navigationBarAppearance.largeTitleTextAttributes = [.foregroundColor:UIColor.white]
+        navigationBarAppearance.titleTextAttributes = [.foregroundColor:UIColor(red: 0.96, green: 0.96, blue: 0.98, alpha: 1.00)]
+        navigationBarAppearance.largeTitleTextAttributes = [.foregroundColor:UIColor(red: 0.96, green: 0.96, blue: 0.98, alpha: 1.00)]
         
-        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().tintColor = UIColor(red: 0.96, green: 0.96, blue: 0.98, alpha: 1.00)
         UINavigationBar.appearance().standardAppearance = navigationBarAppearance
         UINavigationBar.appearance().compactAppearance = navigationBarAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
